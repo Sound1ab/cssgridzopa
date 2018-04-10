4343f6c6ef2492fe0b0da428c8b3c5cefaffeaa7
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-10 16:36:42 +0100

    Update button with additional props

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,14 +1,26 @@
 import styled, {css} from 'styled-components';
+import PropTypes from "prop-types";
 
 const Button = styled.button`
   border: none;
-  padding: 12px 48px;
+  padding: 10px 48px;
   text-transform: uppercase;
   font-size: calc(var(--fz-base) * 1px);
   background-color: var(--pink);
   color: var(--white); 
   border-radius: 5px;
-  ${props => props.noMarginBottom ? css`margin-bottom: 0` : css`margin-bottom: 16px`}
+  ${props => props.marginBottom ? css`margin-bottom: 16px` : css`margin-bottom: 0`}
+  ${props => props.expand ? css`width: 100%` : null}
 `;
+
+Button.propTypes = {
+    marginBottom: PropTypes.bool,
+    expand: PropTypes.bool
+};
+
+Button.defaultProps = {
+    marginBottom: true,
+    expand: false
+};
 
 export default Button;
