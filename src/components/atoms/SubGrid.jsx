97eed84f3083665a79516f8223540b08ec73cc54
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-10 12:24:21 +0100

    Update subgrid to accept more subgrid props

--- src/components/atoms/SubGrid.jsx
+++ src/components/atoms/SubGrid.jsx
@@ -1,18 +1,32 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import PropTypes from "prop-types";
 
 const SubGrid = styled.div`
   display: flex;
   justify-content: ${props => props.justifyContent};
   align-items: ${props => props.alignItems};
-  grid-column: ${props => props.colSpan};
-  grid-row: ${props => props.rowSpan};
+  ${props => props.colStart ? css`grid-column-start: ${props => props.colStart}` : null};
+  ${props => props.colEnd ? css`grid-column-end: ${props => props.colEnd}` : null};
+  ${props => props.colSpan ? css`grid-column: ${props => props.colSpan}` : null};
+  ${props => props.rowStart ? css`grid-row-start: ${props => props.rowStart}` : null};
+  ${props => props.rowEnd ? css`grid-row-end: ${props => props.rowEnd}` : null};
+  ${props => props.rowSpan ? css`grid-row: ${props => props.rowSpan}` : null};
+  ${props => props.gridArea ? css`grid-area: ${props => props.gridArea}` : null};
+  ${props => props.paddingTop ? css`padding-top: ${props.paddingTop}px` : null}
+  ${props => props.paddingBottom ? css`padding-bottom: ${props.paddingBottom}px` : null}
   background-color: ${props => props.backgroundColor};
 `;
 
 SubGrid.propTypes = {
+    paddingTop: PropTypes.number,
+    paddingBottom: PropTypes.number,
+    colStart: PropTypes.string,
+    colEnd: PropTypes.string,
     colSpan: PropTypes.string,
+    rowStart: PropTypes.string,
+    rowEnd: PropTypes.string,
     rowSpan: PropTypes.string,
+    gridArea: PropTypes.string,
     backgroundColor: PropTypes.string,
     justifyContent: function (props, propName) {
         if (!props[propName].match(/(flex-start|flex-end|center|space-between|space-around|space-evenly)/)) {
@@ -27,8 +41,15 @@ SubGrid.propTypes = {
 };
 
 SubGrid.defaultProps = {
-    colSpan: '1 / 1',
-    rowSpan: '1 / 1',
+    paddingTop: 0,
+    paddingBottom: 0,
+    colStart: '',
+    colEnd: '',
+    colSpan: '',
+    rowStart: '',
+    rowEnd: '',
+    rowSpan: '',
+    gridArea: '',
     backgroundColor: 'transparent',
     justifyContent: 'center',
     alignItems: 'center'
