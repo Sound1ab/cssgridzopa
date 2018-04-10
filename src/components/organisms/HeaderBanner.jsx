8546622c570704dcab7eee04bde250e381102cdb
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-10 09:47:51 +0100

    Remove header banner props

--- src/components/organisms/HeaderBanner.jsx
+++ src/components/organisms/HeaderBanner.jsx
@@ -1,23 +1,56 @@
 import React from 'react';
+import Heading from '../atoms/Heading';
+import Grid from '../atoms/Grid';
+import SubGrid from '../atoms/SubGrid';
+import GridBackground from '../atoms/GridBackground';
+import Wrapper from '../atoms/Wrapper';
+import Button from '../atoms/Button';
+import Text from '../atoms/Text';
 import PropTypes from 'prop-types';
 
-const HeaderBanner = ({props}) => {
+const HeaderBanner = () => {
     return (
-
+        <Grid
+            columns={'1fr [main-start] minmax(auto, 650px) [main-end] 1fr'}
+            rows={'1fr 124px 1fr'}
+            height={813}
+        >
+            <GridBackground
+                src={require('../../assets/images/header-photo.jpg')}
+            />
+            <SubGrid
+                colSpan={'main-start / main-end'}
+                rowSpan={'1 / 1'}
+            >
+                <Wrapper
+                    alignItems={'flex-end'}
+                    textAlign={'center'}
+                >
+                    <Heading>There is no other platform for you as like</Heading>
+                </Wrapper>
+            </SubGrid>
+            <SubGrid
+                colSpan={'main-start / main-end'}
+                rowSpan={'2/2'}
+            >
+                <img
+                    src={require('../../assets/images/header-icon-play.png')}
+                />
+            </SubGrid>
+            <SubGrid
+                colSpan={'main-start / main-end'}
+                rowSpan={'3/3'}
+            >
+                <Wrapper
+                    justifyContent={'flex-start'}
+                    flexDirection={'column'}
+                >
+                    <Button>try now</Button>
+                    <Text size={'small'}>* No need to add card details</Text>
+                </Wrapper>
+            </SubGrid>
+        </Grid>
     )
-};
-
-HeaderBanner.propTypes = {
-    testProp: PropTypes.string,
-    customProp: function (props, propName) {
-        if (!props[propName].match(/(h1|h2|h3|h4|h5|h6)/)) {
-            return new Error ('ERROR');
-        }
-    }
-};
-
-HeaderBanner.defaultProps = {
-    testProp: ''
 };
 
 export default HeaderBanner;
