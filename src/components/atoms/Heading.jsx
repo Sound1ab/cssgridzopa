904a08ba883ea4b5f1d0818ec5e781b8b0068628
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-10 15:32:36 +0100

    Update heading to accept color prop

--- src/components/atoms/Heading.jsx
+++ src/components/atoms/Heading.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const StyledHeading = ({type, children, className}) => {
     return React.createElement(type, {className: className}, children);
 };
 
 StyledHeading.propTypes = {
+    color: PropTypes.string,
     type: function (props, propName) {
         if (!props[propName].match(/(h1|h2|h3|h4|h5|h6)/)) {
             return new Error ('Invalid prop type');
@@ -14,10 +16,12 @@ StyledHeading.propTypes = {
 };
 
 StyledHeading.defaultProps = {
+    color: 'var(--black)',
     type: 'h1'
 };
 
 export default styled(StyledHeading)`
+    color: ${props => props.color};
     font-family: var(--ff-heading);
     line-height: calc(var(--lh-base) / var(--fz-base));
 `;
