1dc07b70f09249cf6e02fb234f5eb8499936873b
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-10 16:37:21 +0100

    Add signup component

--- src/components/organisms/SignUp.jsx
+++ src/components/organisms/SignUp.jsx
@@ -5,8 +5,9 @@ import Heading from '../atoms/Heading';
 import Grid from '../atoms/Grid';
 import SubGrid from '../atoms/SubGrid';
 import Wrapper from '../atoms/Wrapper';
-import Text from '../atoms/Text';
+import Input from '../atoms/Input';
 import Button from '../atoms/Button';
+import Text from '../atoms/Text';
 
 let ResponsiveGrid = Grid.extend`
   max-width: 1200px;
@@ -15,7 +16,7 @@ let ResponsiveGrid = Grid.extend`
   grid-template-rows: auto 1fr auto;
   grid-template-areas: 
     "heading heading heading heading"
-    "content-1 content-2 content-3 content-4";
+    "content-1 content-2 content-3 content-4"
     "tc tc tc tc";
   @media (max-width: ${breakpoints.s}px) {
     grid-template-columns: 1fr;
@@ -27,6 +28,23 @@ let ResponsiveGrid = Grid.extend`
       "content-3"
       "content-4"
       "tc";
+  }
+`;
+
+let FloatingText = Text.extend`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  white-space: nowrap;
+  &:before {
+    content: '';
+    width: 20px;
+    height: 27px;
+    background-image: url('${props => props.pseudoImage}');
+    position: absolute;
+    left: -25px;
+    top: 8px;
   }
 `;
 
@@ -47,23 +65,34 @@ const Features = () => {
                     </Wrapper>
                 </SubGrid>
                 <SubGrid colStart={'content-1'}>
-                    <Wrapper flexDirection={'column'} textAlign={'center'}>
-
+                    <Wrapper flexDirection={'column'} textAlign={'center'} padding={[40, 18, 40, 18]}>
+                        <Input type="text" placeholder="full name"/>
                     </Wrapper>
                 </SubGrid>
                 <SubGrid colStart={'content-2'}>
                     <Wrapper flexDirection={'column'} textAlign={'center'}>
-
+                        <Input type="text" placeholder="your email"/>
                     </Wrapper>
                 </SubGrid>
                 <SubGrid colStart={'content-3'}>
                     <Wrapper flexDirection={'column'} textAlign={'center'}>
-
+                        <Input type="text" placeholder="password"/>
                     </Wrapper>
                 </SubGrid>
                 <SubGrid colStart={'content-4'}>
                     <Wrapper flexDirection={'column'} textAlign={'center'}>
-                        <Button noMarginBottom>Try now</Button>
+                        <FloatingText
+                            light={true}
+                            pseudoImage={require('../../assets/images/arrow.png')}
+                        >30 days free trial for all</FloatingText>
+                        <Button marginBottom={false} expand={true}>Try now</Button>
+                    </Wrapper>
+                </SubGrid>
+                <SubGrid gridArea={'tc'}>
+                    <Wrapper>
+                        <Text marginBottom={0}>
+                            By signing up you agree to our terms & services
+                        </Text>
                     </Wrapper>
                 </SubGrid>
             </ResponsiveGrid>
