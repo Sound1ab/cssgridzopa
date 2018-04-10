c264ae19fe9bea6e318b0b9bd4ab138976c356e1
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-10 12:25:45 +0100

    Add new components into app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import HeaderBanner from './components/organisms/HeaderBanner';
 import Introduction from './components/organisms/Introduction';
+import Features from './components/organisms/Features';
+import Responsive from './components/organisms/Responsive';
 import ErrorBoundary from './components/ErrorBoundary';
 
 class App extends Component {
     render() {
         return (
             <ErrorBoundary>
-                {/*<HeaderBanner/>*/}
+                <HeaderBanner/>
                 <Introduction/>
+                <Features/>
+                <Responsive/>
             </ErrorBoundary>
         );
     }
