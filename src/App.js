755ad312b1c0d3255c0c1a1e613c7aa00d8c2aac
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-10 09:49:02 +0100

    Add components to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, {Component} from 'react';
-import {StyledInput} from './components/StyledInput';
-import {StyledText} from './components/StyledText';
-import {StyledTitle} from './components/StyledTitle';
-import styled from 'styled-components';
+import HeaderBanner from './components/organisms/HeaderBanner';
+import Introduction from './components/organisms/Introduction';
 import ErrorBoundary from './components/ErrorBoundary';
 
 class App extends Component {
-    constructor (props) {
-        super(props);
-    }
     render() {
         return (
             <ErrorBoundary>
-                <div className={this.props.className}>
-
-                </div>
+                {/*<HeaderBanner/>*/}
+                <Introduction/>
             </ErrorBoundary>
         );
     }
 }
 
-export default styled(App)`
-
-`;
+export default App;
