b2f0d6973524a24bbcd4751eedf6ad8842f4e011
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-10 09:46:13 +0100

    Add gridbackground component

--- src/components/atoms/GridBackground.jsx
+++ src/components/atoms/GridBackground.jsx
@@ -1,25 +1,32 @@
 import styled from 'styled-components';
 import PropTypes from "prop-types";
+import {css} from 'styled-components';
 
 const GridBackground = styled.div`
-  background-color: red;
-  background-image: url('${props => props.src}');
+  background-color: ${props => props.backgroundColor};
+  ${props => props.src ? css`background-image: url(${props => props.src})` : null};
   background-size: cover;
   background-position: center;
   position: absolute;
   width: 100%;
   height: 100%;
-  grid-row: 1 / -1;
-  grid-column: 1 / -1;
+  grid-row: ${props => props.rowSpan};
+  grid-column: ${props => props.colSpan};
   z-index: -1;
 `;
 
 
 GridBackground.propTypes = {
+    colSpan: PropTypes.string,
+    rowSpan: PropTypes.string,
+    backgroundColor: PropTypes.string,
     src: PropTypes.string
 };
 
 GridBackground.defaultProps = {
+    colSpan: '1 / 1',
+    rowSpan: '1 / 1',
+    backgroundColor: 'var(--pink)',
     src: ''
 };
 
